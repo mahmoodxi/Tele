d3aa40c7a0ff532149b7f1cbc90db53df0def220
mahmoodxi <36129031+mahmoodxi@users.noreply.github.com>
2020-02-21 16:17:10 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,30 @@
+PORT = 443
+
+# name -> secret (32 hex chars)
+USERS = {
+    "tg":  "8a688e49b2cad1299445f0c23a6a581d",
+    # "tg2": "0123456789abcdef0123456789abcdef",
+}
+
+MODES = {
+    # Classic mode, easy to detect
+    "classic": False,
+
+    # Makes the proxy harder to detect
+    # Can be incompatible with very old clients
+    "secure": False,
+
+    # Makes the proxy even more hard to detect
+    # Can be incompatible with old clients
+    "tls": True
+}
+
+# The domain for TLS mode, bad clients are proxied there
+# Use random existing domain, proxy checks it on start
+TLS_DOMAIN = "i.instagram.com"
+
+# Tag for advertising, obtainable from @MTProxybot
+AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
+
+TO_CLT_BUFSIZE = 262144
+TO_TG_BUFSIZE = 262144
